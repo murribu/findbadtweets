dcbbed0d6e57f0deb0c8cb0b6c4f33a3a7d2bcfd
Cory Martin <cory.martin@dozensoft.com>
2019-04-19 23:16:35 -0500

    more cdk

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -2,12 +2,14 @@ import cdk = require("@aws-cdk/cdk");
 import dynamodb = require("@aws-cdk/aws-dynamodb");
 import appsync = require("@aws-cdk/aws-appsync");
 import iam = require("@aws-cdk/aws-iam");
+import fs = require("fs");
 
-// import {
-//   CfnUserPool,
-//   CfnUserPoolClient,
-//   CfnIdentityPool
-// } from "@aws-cdk/aws-cognito";
+import {
+  CfnUserPool,
+  CfnUserPoolClient,
+  CfnIdentityPool,
+  CfnIdentityPoolRoleAttachment
+} from "@aws-cdk/aws-cognito";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -25,7 +27,7 @@ export class CdkStack extends cdk.Stack {
       name: "fbt_api"
     });
 
-    const iam_role = new iam.CfnRole(this, "fbt_ds_role", {
+    const iam_ds_role = new iam.CfnRole(this, "fbt_ds_role", {
       roleName: "appsync-ds" + api.graphQlApiApiId + "-" + user_table.tableName,
       assumeRolePolicyDocument: {
         Version: "2012-10-17",
@@ -71,13 +73,67 @@ export class CdkStack extends cdk.Stack {
         tableName: user_table.tableName,
         awsRegion: "us-east-1"
       },
-      serviceRoleArn: iam_role.roleArn
+      serviceRoleArn: iam_ds_role.roleArn
     });
 
-    console.log(user_table);
-    console.log(datasource);
+    const schemadefinition = fs.readFileSync(
+      `${__dirname}/../graphql/schema.graphql`,
+      {
+        encoding: "utf-8"
+      }
+    );
 
-    /*
+    const fbtschema = new appsync.CfnGraphQLSchema(this, "fbt_api_schema", {
+      apiId: api.graphQlApiApiId,
+      definition: schemadefinition
+    });
+
+    const fbtUpdateUserResponseResolver = fs.readFileSync(
+      `${__dirname}/../graphql/resolvers/updateUser.response.resolver`,
+      { encoding: "utf-8" }
+    );
+
+    const fbtUpdateUserRequestResolver = fs.readFileSync(
+      `${__dirname}/../graphql/resolvers/updateUser.request.resolver`,
+      { encoding: "utf-8" }
+    );
+
+    const fbtUpdateUserResolver = new appsync.CfnResolver(
+      this,
+      "fbt_update_user_resolver",
+      {
+        apiId: api.graphQlApiApiId,
+        fieldName: "updateUser",
+        typeName: "Mutation",
+        dataSourceName: datasource.dataSourceName,
+        requestMappingTemplate: fbtUpdateUserRequestResolver,
+        responseMappingTemplate: fbtUpdateUserResponseResolver
+      }
+    );
+
+    const fbtGetMyProfileResponseResolver = fs.readFileSync(
+      `${__dirname}/../graphql/resolvers/getMyProfile.response.resolver`,
+      { encoding: "utf-8" }
+    );
+
+    const fbtGetMyProfileRequestResolver = fs.readFileSync(
+      `${__dirname}/../graphql/resolvers/getMyProfile.request.resolver`,
+      { encoding: "utf-8" }
+    );
+
+    const fbtGetMyProfileResolver = new appsync.CfnResolver(
+      this,
+      "fbt_get_my_profile_resolver",
+      {
+        apiId: api.graphQlApiApiId,
+        fieldName: "getMyProfile",
+        typeName: "Query",
+        dataSourceName: datasource.dataSourceName,
+        requestMappingTemplate: fbtGetMyProfileRequestResolver,
+        responseMappingTemplate: fbtGetMyProfileResponseResolver
+      }
+    );
+
     const userPool = new CfnUserPool(this, "fbtuserpool", {
       policies: {
         passwordPolicy: {
@@ -90,13 +146,13 @@ export class CdkStack extends cdk.Stack {
       }
     });
 
-    const userPoolClient = new CfnUserPoolClient(this, "fbtuserpoolclient", {
+    const userPoolClient = new CfnUserPoolClient(this, "fbt_userpoolclient", {
       generateSecret: false,
       clientName: "web",
       userPoolId: userPool.userPoolId
     });
 
-    new CfnIdentityPool(this, "findbadtweets_ip", {
+    const identityPool = new CfnIdentityPool(this, "fbt_identitypool", {
       allowUnauthenticatedIdentities: false,
       cognitoIdentityProviders: [
         {
@@ -104,6 +160,112 @@ export class CdkStack extends cdk.Stack {
           providerName: userPool.userPoolProviderName
         }
       ]
-    });*/
+    });
+
+    const iam_appsync_authrole = new iam.CfnRole(this, "fbt_appsync_authrole", {
+      roleName: "appsync-authrole-" + api.graphQlApiApiId,
+      assumeRolePolicyDocument: {
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Effect: "Allow",
+            Principal: {
+              Federated: "cognito-identity.amazonaws.com"
+            },
+            Action: "sts:AssumeRoleWithWebIdentity",
+            Condition: {
+              StringEquals: {
+                "cognito-identity.amazonaws.com:aud":
+                  identityPool.identityPoolId
+              },
+              "ForAnyValue:StringLike": {
+                "cognito-identity.amazonaws.com:amr": "authenticated"
+              }
+            }
+          }
+        ]
+      },
+      policies: [
+        {
+          policyName: "FindBadTweetsAppsyncPolicy",
+          policyDocument: {
+            Version: "2012-10-17",
+            Statement: [
+              {
+                Sid: "VisualEditor0",
+                Effect: "Allow",
+                Action: "appsync:GraphQL",
+                Resource: [
+                  api.graphQlApiArn,
+                  "arn:aws:appsync:*:*:apis/*/types/*/fields/*"
+                ]
+              }
+            ]
+          }
+        }
+      ]
+    });
+
+    const iam_appsync_unauthrole = new iam.CfnRole(
+      this,
+      "fbt_appsync_unauthrole",
+      {
+        roleName: "appsync-unauthrole-" + api.graphQlApiApiId,
+        assumeRolePolicyDocument: {
+          Version: "2012-10-17",
+          Statement: [
+            {
+              Effect: "Allow",
+              Principal: {
+                Federated: "cognito-identity.amazonaws.com"
+              },
+              Action: "sts:AssumeRoleWithWebIdentity",
+              Condition: {
+                StringEquals: {
+                  "cognito-identity.amazonaws.com:aud":
+                    identityPool.identityPoolId
+                },
+                "ForAnyValue:StringLike": {
+                  "cognito-identity.amazonaws.com:amr": "unauthenticated"
+                }
+              }
+            }
+          ]
+        },
+        policies: [
+          {
+            policyName: "FindBadTweetsAppsyncPolicy",
+            policyDocument: {
+              Version: "2012-10-17",
+              Statement: [
+                {
+                  Effect: "Allow",
+                  Action: ["mobileanalytics:PutEvents", "cognito-sync:*"],
+                  Resource: ["*"]
+                }
+              ]
+            }
+          }
+        ]
+      }
+    );
+
+    const identityPoolRoleAttachment = new CfnIdentityPoolRoleAttachment(
+      this,
+      "fbt_identitypoolroleattachment",
+      {
+        identityPoolId: identityPool.identityPoolId,
+        roles: {
+          authenticated: iam_appsync_authrole.roleArn,
+          unauthenticated: iam_appsync_unauthrole.roleArn
+        }
+      }
+    );
+
+    console.log(identityPoolRoleAttachment);
+    console.log(identityPool);
+    console.log(fbtUpdateUserResolver);
+    console.log(fbtGetMyProfileResolver);
+    console.log(fbtschema);
   }
 }
