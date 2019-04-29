9eb277c35f7e93e84bda487b71c25ee7eacf8b8e
Cory Martin <cory.martin@dozensoft.com>
2019-04-29 08:34:53 -0500

    add TopNav test

--- src/Components/TopNav.test.js
+++ src/Components/TopNav.test.js
@@ -1,16 +1,21 @@
 import React from "react";
 import TopNav from "./TopNav";
 
+const props = {
+  handleUserSignOut: jest.fn(),
+  sub: "ASDF-4561",
+  profile: {
+    displayName: "Josh Lymon",
+    email: "jlymon@whitehouse.gov"
+  }
+};
+
 describe("TopNav Component", () => {
   it("should load without crashing", () => {
-    const props = {
-      handleUserSignOut: jest.fn(),
-      sub: "ASDF-4561",
-      profile: {
-        displayName: "Josh Lymon",
-        email: "jlymon@whitehouse.gov"
-      }
-    };
+    const wrapper = shallow(<TopNav />);
+  });
+  it("when logged in, should show 'welcome'", () => {
     const wrapper = shallow(<TopNav {...props} />);
+    expect(wrapper.text()).toMatch(/Welcome/);
   });
 });
